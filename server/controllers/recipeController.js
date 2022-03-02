33f6b2fe786d94bd55fb4999998646b65e32be5c
midoritiba <mic.tiba@gmail.com>
2022-03-02 15:15:35 +0100

    database connected

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,10 +1,32 @@
+require('../models/database.js')
+const Category = require('../models/Category')
 
 /* homepage GET */
-
 
 exports.homepage = async(req, res) => {
 
   res.render('index', {title: ' Cooking Blog | Home'});
 
+}
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+      {
+        name: "Chinese",
+        image: "chinese_food.png"
+      },
+      {name: "Thai",
+        image: "thai_food.png"
+      },
+      {name: "Mexican",
+        image: "mexican_food.png"
+      }
+    ]);
+  } catch (error) {
+    console.log('err', + error)
+  }
 
 }
+
+insertDummyCategoryData();

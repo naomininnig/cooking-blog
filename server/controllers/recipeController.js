e943bec81a3a7505f5948d92324baea92fe969c0
midoritiba <mic.tiba@gmail.com>
2022-03-04 13:53:54 +0100

    Index category fixed

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -11,10 +11,10 @@ exports.homepage = async(req, res) => {
     const bbq = await Recipe.find({ 'category': "BBQ & Grill"}).limit(limitNumber);
     const mains = await Recipe.find({ 'category': 'Mains'}).limit(limitNumber);
     const pasta = await Recipe.find({ 'category': 'Pasta'}).limit(limitNumber);
-    const snacks = await Recipe.find({ 'category': 'Snack'}).limit(limitNumber);
+    const snacks = await Recipe.find({ 'category': 'Snacks'}).limit(limitNumber);
     const starters = await Recipe.find({ 'category': 'Starters'}).limit(limitNumber);
     const breakfast = await Recipe.find({ 'category': 'Breakfast'}).limit(limitNumber);
-    const dessert = await Recipe.find({ 'category': 'Dessert'}).limit(limitNumber);
+    const dessert = await Recipe.find({ 'category': 'Desserts'}).limit(limitNumber);
     const food = { latest, bbq, mains, pasta, snacks, starters, breakfast, dessert };
     res.render('index', {title: ' Cooking Blog | Home', categories, food});
   } catch (error) {

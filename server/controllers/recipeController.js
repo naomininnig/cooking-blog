c987328191de475a32c7c1bb9ff57161c3455f57
midoritiba <mic.tiba@gmail.com>
2022-03-04 13:30:23 +0100

    categories id fixed, categories by id working

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -8,13 +8,13 @@ exports.homepage = async(req, res) => {
     const limitNumber = 5
     const categories = await Category.find({}).limit(limitNumber)
     const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
-    const bbq = await Recipe.find({ 'category': 'BBQ & Grill'}).limit(limitNumber);
+    const bbq = await Recipe.find({ 'category': "BBQ & Grill"}).limit(limitNumber);
     const mains = await Recipe.find({ 'category': 'Mains'}).limit(limitNumber);
     const pasta = await Recipe.find({ 'category': 'Pasta'}).limit(limitNumber);
-    const snacks = await Recipe.find({ 'category': 'Snacks'}).limit(limitNumber);
+    const snacks = await Recipe.find({ 'category': 'Snack'}).limit(limitNumber);
     const starters = await Recipe.find({ 'category': 'Starters'}).limit(limitNumber);
     const breakfast = await Recipe.find({ 'category': 'Breakfast'}).limit(limitNumber);
-    const dessert = await Recipe.find({ 'category': 'Desserts'}).limit(limitNumber);
+    const dessert = await Recipe.find({ 'category': 'Dessert'}).limit(limitNumber);
     const food = { latest, bbq, mains, pasta, snacks, starters, breakfast, dessert };
     res.render('index', {title: ' Cooking Blog | Home', categories, food});
   } catch (error) {
@@ -36,9 +36,9 @@ exports.exploreCategories = async(req, res) => {
 /* categories by ID  GET */
 exports.exploreCategoriesById = async(req, res) => {
   try {
-    let categoryId = req.params.id
+    const categoryId = req.params.id
     const limitNumber = 20;
-    const categoryById= await Recipe.find({'category': categoryById}).limit(limitNumber)
+    const categoryById= await Recipe.find({'category': categoryId}).limit(limitNumber)
     res.render('categories', {title: ' Cooking Blog | Categories', categoryById});
   } catch (error) {
     res.status(400).send({message: error.message || "Oopsi, error :("})
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -8,8 +8,8 @@ const recipeController = require('../controllers/recipeController');
  */
 
 router.get('/', recipeController.homepage);
-router.get('/categories', recipeController.exploreCategories)
 router.get('/recipe/:id', recipeController.exploreRecipe)
+router.get('/categories', recipeController.exploreCategories)
 router.get('/categories/:id', recipeController.exploreCategoriesById)
 
 

27192adc83d325fa574f8bb11d1383754a01b686
midoritiba <mic.tiba@gmail.com>
2022-03-04 11:25:19 +0100

    categories link fixed

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -27,7 +27,7 @@ exports.exploreCategories = async(req, res) => {
   try {
     const limitNumber = 20;
     const categories = await Category.find({}).limit(limitNumber)
-    res.render('categories', {title: ' Cooking Blog | Categories', categories, food});
+    res.render('categories', {title: ' Cooking Blog | Categories', categories});
   } catch (error) {
     res.status(400).send({message: error.message || "Oopsi, error :("})
   }
